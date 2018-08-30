ec05f1ed725455d226ffc87533e477169c8ef096
xcQuence <johnyteen@gmail.com>
2018-08-30 11:44:38 -0400

    revised database

--- todolistproject/main.py
+++ todolistproject/main.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template, request, json
-import sqlite3
+from flask import Flask, render_template, request
+import sqlite3 as sql
 app = Flask(__name__)
 
-conn = sqlite3.connect('todolist.db')
-print "Opened Database Successfully"
 
-conn.close()
+
+
 
 @app.route("/")
-
 def main():
     return render_template('index.html')
 
@@ -17,12 +15,30 @@ def showSignUp():
     return render_template('signup.html')
 
 @app.route('/signUp', methods=['GET', 'POST'])
-def signUp() :
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
+def signUp():
+    if request.method == 'POST':
+        try:
+            _name = request.form['name']
+            _email = request.form['email']
+            _password = request.form['password']
+
+            with sql.connect('users1') as conn:
+                cur = conn.cursor()
+                cur.execute("INSERT INTO user(name, email, password) VALUES (?,?,?)", (_name, _email, _password))
+                conn.commit()
+                msg = "Successfully registered"
+        except:
+             conn.rollback()
+             msg = "error in insert operation"
+
+        finally:
+
+            return render_template("test.html", msg = msg )
+            conn.close()
+
+
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
